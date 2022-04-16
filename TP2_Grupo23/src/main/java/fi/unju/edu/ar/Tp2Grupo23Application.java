d21334b75b9af30d3db456a0f281a3389abef6fa
johnrm992 <johnrm992@johnrm992-C14D>
2022-04-16 19:12:27 -0300

    Prueba 1

---
+++ TP2_Grupo23/src/main/java/fi/unju/edu/ar/Tp2Grupo23Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo23Application.class, args);
+	}
+
+}
