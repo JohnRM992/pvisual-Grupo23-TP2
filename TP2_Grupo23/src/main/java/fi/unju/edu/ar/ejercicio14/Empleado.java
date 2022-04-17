390a2ef131f903b084b4d2f96455e2b2e1357d6b
johnrm992 <johnrm992@johnrm992-C14D>
2022-04-16 21:52:19 -0300

    Punto13 y 14 estaban intercambiados

--- TP2_Grupo23/src/main/java/fi/unju/edu/ar/ejercicio14/Empleado.java
+++ TP2_Grupo23/src/main/java/fi/unju/edu/ar/ejercicio14/Empleado.java
@@ -1,57 +1,39 @@
 package fi.unju.edu.ar.ejercicio14;
 
+import java.util.Date;
 import java.util.Scanner;
 public class Empleado {
 
+	private String nombre;
+	private String email;
+	private String fechaIngreso;
+	private int legajo;
+	private int horasTrabajadas;
 	public static void main(String[] args) {
-		try{
-			Scanner lectura = new Scanner(System.in);
-			System.out.println("Ingrese su nombre: ");
-			
-			String nombre = lectura.nextLine();
-			
-			System.out.println("Ingrese su correo: ");
-			
-			String email = lectura.nextLine();
-			
-			System.out.println("Ingrese su fecha de ingreso (dd/mm/aaaa): ");
-			
-			String fechaIngreso= lectura.nextLine();
-			
-			System.out.println("Ingrese su numero de legajo: ");
-			
-			int legajo = lectura.nextInt();
-			
-			System.out.println("Ingrese la cantidad de horas trabajadas: ");
-			int horasTrabajadas = lectura.nextInt();
-			
-			int pagoPorHora1= 600;
-			int pagoPorHora2= 650;
-			float sueldo;
-			
-			
-			if(horasTrabajadas>160) {
-				sueldo = (pagoPorHora1*horasTrabajadas); 
-			}
-			else {
-				sueldo = (pagoPorHora2*horasTrabajadas);
-			}
-			
-			lectura.close();
-			
-			System.out.println("#######Tus datos#######:");
-			System.out.println("Nombre: "+nombre);
-			System.out.println("Legajo n°: "+legajo);
-			System.out.println("Correo: "+email);
-			System.out.println("Fecha de ingreso: "+fechaIngreso);
-			System.out.println("Tu sueldo es: $"+sueldo);
-			
-
-		}
-		catch
-			(Exception e){System.out.println("Error en alguno de los campos ingresados");
-		}
 		
-	} //fin de main
-
+	
+		
+	}
+	public Empleado() {
+		super();
+	}
+	public Empleado(String nombre, int legajo, int horasTrabajadas) {
+		super();
+		this.nombre = nombre;
+		this.legajo = legajo;
+		this.horasTrabajadas = horasTrabajadas;
+	}
+	public Empleado(String nombre, String fechaIngreso, int horasTrabajadas) {
+		super();
+		this.nombre = nombre;
+		this.fechaIngreso = fechaIngreso;
+		this.horasTrabajadas = horasTrabajadas;
+	}
+	public Empleado(String nombre, int horasTrabajadas) {
+		super();
+		this.nombre = nombre;
+		this.horasTrabajadas = horasTrabajadas;
+	}
+		Scanner lectura = new Scanner(System.in);
+		
 }
