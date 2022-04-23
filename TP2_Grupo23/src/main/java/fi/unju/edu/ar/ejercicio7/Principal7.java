b76a8e88fcfcbca4fa436a62169e9a28c38316e7
joel_ <joel_@192.168.0.124>
2022-04-23 19:24:53 -0300

    Ejercicio 7

--- TP2_Grupo23/src/main/java/fi/unju/edu/ar/ejercicio7/Principal7.java
+++ TP2_Grupo23/src/main/java/fi/unju/edu/ar/ejercicio7/Principal7.java
@@ -2,11 +2,12 @@ package fi.unju.edu.ar.ejercicio7;
 
 public class Principal7 {
 	public static void main(String[] args){
-		for(int i = 2; i <= 441; i = i + 37) {
-			System.out.println("i " + i);
-			for(int j = 40; j < 0; j = j * i) {
-				System.out.println("j " + j);
-			}
+		System.out.println("Holaaa");
+	
+		int j = 40;
+		for (int i = 2; i <= 41; i++) {
+			System.out.println(i*j);
+			j--;
 		}
 	}
 }
